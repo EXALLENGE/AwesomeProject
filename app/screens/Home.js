ed7342970fa1cf203e8be065720d775dbf9f413b
Fedor <arbuzovfp@gmail.com>
2019-09-17 22:05:35 +0300

    fix

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -2,11 +2,10 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import { Container } from "../components/Container";
+import { Logo } from "../components/Logo";
 
 export default () => (
   <Container>
-    <View>
-      <Text>hello</Text>
-    </View>
+    <Logo />
   </Container>
 );
