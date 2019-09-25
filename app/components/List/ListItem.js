d3dd3cfdf8c0d072aa06b710be6c8be1fc58a055
Fedor <arbuzovfp@gmail.com>
2019-09-25 21:00:21 +0300

    fix

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -9,12 +9,14 @@ const ListItem = ({
   onPress,
   selected = false,
   checkmark = true,
-  visible = true
+  visible = true,
+  customIcon = null
 }) => (
   <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
       {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+      {customIcon}
     </View>
   </TouchableHighlight>
 );
