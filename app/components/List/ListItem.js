9d97a126a8f7fd1154a68c31022a79d55f581e5f
Fedor <arbuzovfp@gmail.com>
2019-09-25 22:07:13 +0300

    fix

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -10,12 +10,21 @@ const ListItem = ({
   selected = false,
   checkmark = true,
   visible = true,
-  customIcon = null
+  customIcon = null,
+  iconBackground = null
 }) => (
   <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
-      {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon />}
+      {selected ? (
+        <Icon
+          checkmark={checkmark}
+          visible={visible}
+          iconBackground={iconBackground}
+        />
+      ) : (
+        <Icon />
+      )}
       {customIcon}
     </View>
   </TouchableHighlight>
@@ -26,7 +35,8 @@ ListItem.propTypes = {
   onPress: PropTypes.func,
   selected: PropTypes.bool,
   checkmark: PropTypes.bool,
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
+  iconBackground: PropTypes.string
 };
 
 export default ListItem;
--- app/index.js
+++ app/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 
-import Options from "./screens/Options";
+import Themes from "./screens/Themes";
 
 EStyleSheet.build({
   $primaryBlue: "#4F6D7A",
+  $primaryOrange: "#D57A66",
+  $primaryGreen: "#00BD9D",
+  $primaryPurple: "#9E768F",
+
   $white: "#ffffff",
   $border: "#E2E2E2",
   $inputText: "#797979",
   $darkText: "#343434"
 });
 
-export default () => <Options />;
+export default () => <Themes />;
